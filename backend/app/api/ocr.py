4751e075a261893d7bd44d557e0cd6a0ef08cdfc
Philip-Cyber28 <pacg816@gmail.com>
2025-12-17 19:06:22 +0400

    Fix OCR endpoint for production on Render

--- backend/app/api/ocr.py
+++ backend/app/api/ocr.py
@@ -1,5 +1,7 @@
 import shutil
-from fastapi import APIRouter, UploadFile, File, Depends
+import os
+import tempfile
+from fastapi import APIRouter, UploadFile, File, Depends, HTTPException
 from app.ml.ocr import run_ocr
 from app.ml.classifier import predict_from_receipt
 from app.services.dependencies import get_current_user
@@ -8,18 +10,28 @@ router = APIRouter(tags=["OCR"])
 
 @router.post("/ocr")
 def ocr_receipt(
-    file : UploadFile = File(...),
+    file: UploadFile = File(...),
     current_user: str = Depends(get_current_user),
 ):
-    file_path = f"temp_{file.filename}"
-    with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+
+    try:
+        temp_dir = tempfile.gettempdir()
+        file_path = os.path.join(temp_dir, file.filename)
+
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        text = run_ocr(file_path)
+
+        categorized_items = predict_from_receipt(text)
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"OCR processing failed: {e}")
     
-    text = run_ocr(file_path)
+    finally:
+        if os.path.exists(file_path):
+            os.remove(file_path)
 
-    categorized_items = predict_from_receipt(text)
-
-    return{
+    return {
         "ocr_text": text,
         "predicted_items": categorized_items
     }
