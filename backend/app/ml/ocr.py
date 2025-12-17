7ae51e834b13426e842d8c91bdf0aae4297b1443
Philip-Cyber28 <pacg816@gmail.com>
2025-12-17 21:02:32 +0400

    OCR fixed

--- backend/app/ml/ocr.py
+++ backend/app/ml/ocr.py
@@ -1,7 +1,18 @@
 import pytesseract
 from PIL import Image, ImageOps
+import platform
+import shutil
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+if platform.system() == 'Windows':
+    # WINDOWS PATH
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+else:
+    # LINUX/DOCKER
+    tesseract_path = shutil.which('tesseract')
+    if tesseract_path:
+        pytesseract.pytesseract.tesseract_cmd = tesseract_path
+    else:
+        pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 
 def run_ocr(image_path: str) -> str:
     img = Image.open(image_path)
